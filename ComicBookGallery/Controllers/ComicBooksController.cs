98fcff79ca85b6ba3b5d761713ea37bd8ea4b2e7
Redem Alben Vinuya <arvivinuya@gmail.com>
2021-11-14 18:54:04 +1300

    Added Detail action method

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,6 +11,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
