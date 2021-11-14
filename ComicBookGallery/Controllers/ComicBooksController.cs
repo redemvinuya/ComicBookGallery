cc27724cb96de61b84413c306a14c886c688aab8
Redem Alben Vinuya <arvivinuya@gmail.com>
2021-11-14 18:51:44 +1300

    Updated ComicBooksController

---
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,16 @@
+﻿using ComicBookGallery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+
+    }
+}
